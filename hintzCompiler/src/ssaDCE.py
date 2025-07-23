eabf3cfc12b1bd750191ad3a368a8186fb37e887
Kumar Aakash <kumar.aakash0229@gmail.com>
2025-07-23 13:44:07 -0400

    Adding the missing files from last push.

---
+++ hintzCompiler/src/ssaDCE.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2024–2025 Kumar Aakash. Released under the MIT License.
+
+from hintzCompiler.src.EditCfg import *
+from hintzCompiler.src.ir_nodes import *
+from hintzCompiler.src.cfg import ControlFlowGraph
+from hintzCompiler.src.readWriteAnalyzer import ReadWriteAnalyzer
+from hintzCompiler.src.PredicatedIRNodeIterator import IRNodeIterator
+
+
+class SSAAwareDeadCodeElimination():
+
+    #public:
+
+    def __init__(self, cfg:ControlFlowGraph):
+        self._cfg = cfg
+        self._rootStmtIDList : List[int] = []
+        self._markedStmtIDList : List[int] = []
+        self._rwa : ReadWriteAnalyzer = ReadWriteAnalyzer(cfg)
+
+    def doit(self):
+        if self._cfg.fcn._isInSSA == False:
+            raise RuntimeError("SSA Aware DeadCodeElimination can only be run on Fcn which are already in SSA form.")
+
+        self.determineRoots()
+        self.mark()
+        self.sweep()
+
+    def xformName(self):
+        return "SSA-Aware-DCE"
+
+    #private:
+
+    def determineRoots(self):
+        # control flow roots
+        for cfgNode in self._cfg.nodes: 
+            n = cfgNode.stmt
+            if isinstance(n, (If, IfJoin, While, DoWhile, DoJoin, For, Declaration)): 
+                self._rootStmtIDList.append(cfgNode.id)
+        # data flow roots
+        for eachNode in IRNodeIterator(self._cfg.fcn, lambda n: isinstance(n, Return)):
+            self._rootStmtIDList.append(eachNode.rootStmt()._cfgNodeId)
+
+    def traverseBackwardsInDataFlow(self, liveStmtId):
+        self._markedStmtIDList.append(liveStmtId)
+        liveStmtReads = self._rwa.get_reads(liveStmtId)
+        for eachLiveRead in liveStmtReads:
+            reachingDefForLiveRead = eachLiveRead.irVarAccessNode._ssaReachingDef
+            if reachingDefForLiveRead is not None:
+                self.traverseBackwardsInDataFlow(reachingDefForLiveRead.rootStmt()._cfgNodeId)
+
+    def startMarkingFrom(self, stmtID):
+        self.traverseBackwardsInDataFlow(stmtID)
+
+    def mark(self):
+        for eachS in self._rootStmtIDList:
+            self.startMarkingFrom(eachS)
+
+    def sweep(self):
+        allNodes  = [x.id for x in self._cfg.nodes]
+        liveNodes = self._markedStmtIDList
+        deadNodes = [x for x in allNodes if x not in liveNodes]
+
+        #print(f"\nLIVE-STMT-IDs: {liveNodes}")
+        #print(f"\nDEAD-STMT-IDs: {deadNodes}")
+
+        for deadStmtId in deadNodes:
+            EditCfg.deleteNode(self._cfg, deadStmtId)
---
+++ hintzCompiler/tests/test_ssa_dce.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2024–2025 Kumar Aakash. Released under the MIT License.
+
+import unittest
+from io import StringIO
+from unittest.mock import patch
+from hintzCompiler.src.ir_nodes import *
+from hintzCompiler.src.cfg import ControlFlowGraph
+from hintzCompiler.src.dominators import Dominators
+from hintzCompiler.src.ssaConverter import SSAConverter
+from hintzCompiler.compiler import buildCompilationContext
+from hintzCompiler.src.ssaDCE import SSAAwareDeadCodeElimination
+from hintzCompiler.src.dominanceFrontier import DominanceFrontiers
+
+
+class TestSSADce(unittest.TestCase):
+
+    def cfgToString(self, cfg:ControlFlowGraph) -> str:
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            cfg.dump()
+            return mock_stdout.getvalue().strip()
+
+    def test_toSSA_basic(self):
+        code = """
+        int main(int in) {
+            int x;
+            int out;
+
+            x = 0;
+            if(in > 5) {
+                x = 1;
+            } else {
+                x = 2;
+            }
+
+            out = x;
+            return out;
+        }
+        """
+
+        expected = """Fcn : main
+[0] Declaration: [Variable(name='x', type_spec='int', attributes={})] -> 1
+[1] Declaration: [Variable(name='out', type_spec='int', attributes={})] -> 3
+[3] If BinaryOp(op=Token('GT_OP', '>'), left=VarAccess(name='in'), right=Literal(value=5.0)) -> 5, 6
+[4] IfJoin() -> 9
+[5] Assignment(target=VarAccess(name='x2'), value=Literal(value=1.0)) -> 4
+[6] Assignment(target=VarAccess(name='x4'), value=Literal(value=2.0)) -> 4
+[7] Assignment(target=VarAccess(name='out1'), value=VarAccess(name='x3')) -> 8
+[8] Return(value=VarAccess(name='out1')) ->
+[9] Assignment(target=VarAccess(name='x3'), value=FunctionCall(name='phi', args=[VarAccess(name='x2'), VarAccess(name='x4')])) -> 7"""
+
+        cctx = buildCompilationContext(code)
+
+        bbg0 = cctx.bbgs[0]
+        doms  = Dominators(bbg0)
+        domFs = DominanceFrontiers(doms)
+
+        toSSA = SSAConverter(domFs)
+        toSSA.doit()
+
+        ssaDce = SSAAwareDeadCodeElimination(cctx.cfgs[0])
+        ssaDce.doit()
+
+        cfgStr = self.cfgToString(cctx.cfgs[0])
+        self.assertIn(expected.strip(), cfgStr, msg=f"\n\n[[-- FAILED --]]\nExpected:||{expected.strip()}||\n\nActual:||{cfgStr}||")
+
